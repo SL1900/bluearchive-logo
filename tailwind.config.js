f17a9d5564816afaf3cd1511d83ec9e619cd8fcf
SL1900 <david_super@inbox.ru>
2024-12-24 23:27:39 +0900

    Update?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+import daisyui from 'daisyui';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{jsx,js,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
   daisyui: {
     themes: [
       {
